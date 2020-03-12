cc404f2dcbd9eb311f4ac21d8b7c265357dbaa71
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2020-03-12 17:17:46 -0300

    salvando no localStorage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './config/ReactotronConfig';
 
@@ -8,16 +9,18 @@ import GlobalStyle from './styles/globals';
 import Header from './components/Header';
 import Routes from './routes';
 
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Header />
-        <Routes />
-        <GlobalStyle />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Header />
+          <Routes />
+          <GlobalStyle />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,20 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './modules/rootReducer';
+
+const persistConfig = {
+  key: 'orulo_favorites',
+  storage,
+};
 
 const enhancer =
   process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
 
-const store = createStore(rootReducer, enhancer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default store;
+const store = createStore(persistedReducer, enhancer);
+const persistor = persistStore(store);
+
+export { store, persistor };
