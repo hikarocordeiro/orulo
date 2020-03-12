eebad09ed6eec2281e0b5c23d8ad58bb0045a8a4
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2020-03-12 09:12:03 -0300

    estilização Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,125 @@
 import React from 'react';
+import {
+  MdFavorite,
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { RealtyList, Pagination } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <RealtyList>
+        <li>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSi_pt5WNuJG0fkz_stvZACU8VDQ_4ycmBuqWAnYxlbRgyUHKqO"
+            alt="Imóvel"
+          />
+          <strong>Claudia Apto ISPGYZ</strong>
+          <span>Porto Alegre / RS</span>
+
+          <button type="button">
+            <div>
+              <MdFavorite size={16} color="#FFF" />
+            </div>
+
+            <span>ADICIONAR AOS FAVORITOS</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSi_pt5WNuJG0fkz_stvZACU8VDQ_4ycmBuqWAnYxlbRgyUHKqO"
+            alt="Imóvel"
+          />
+          <strong>Claudia Apto ISPGYZ</strong>
+          <span>Porto Alegre / RS</span>
+
+          <button type="button">
+            <div>
+              <MdFavorite size={16} color="#FFF" />
+            </div>
+
+            <span>ADICIONAR AOS FAVORITOS</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSi_pt5WNuJG0fkz_stvZACU8VDQ_4ycmBuqWAnYxlbRgyUHKqO"
+            alt="Imóvel"
+          />
+          <strong>Claudia Apto ISPGYZ</strong>
+          <span>Porto Alegre / RS</span>
+
+          <button type="button">
+            <div>
+              <MdFavorite size={16} color="#FFF" />
+            </div>
+
+            <span>ADICIONAR AOS FAVORITOS</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSi_pt5WNuJG0fkz_stvZACU8VDQ_4ycmBuqWAnYxlbRgyUHKqO"
+            alt="Imóvel"
+          />
+          <strong>Claudia Apto ISPGYZ</strong>
+          <span>Porto Alegre / RS</span>
+
+          <button type="button">
+            <div>
+              <MdFavorite size={16} color="#FFF" />
+            </div>
+
+            <span>ADICIONAR AOS FAVORITOS</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSi_pt5WNuJG0fkz_stvZACU8VDQ_4ycmBuqWAnYxlbRgyUHKqO"
+            alt="Imóvel"
+          />
+          <strong>Claudia Apto ISPGYZ</strong>
+          <span>Porto Alegre / RS</span>
+
+          <button type="button">
+            <div>
+              <MdFavorite size={16} color="#FFF" />
+            </div>
+
+            <span>ADICIONAR AOS FAVORITOS</span>
+          </button>
+        </li>
+        <li>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSi_pt5WNuJG0fkz_stvZACU8VDQ_4ycmBuqWAnYxlbRgyUHKqO"
+            alt="Imóvel"
+          />
+          <strong>Claudia Apto ISPGYZ</strong>
+          <span>Porto Alegre / RS</span>
+
+          <button type="button">
+            <div>
+              <MdFavorite size={16} color="#FFF" />
+            </div>
+
+            <span>ADICIONAR AOS FAVORITOS</span>
+          </button>
+        </li>
+      </RealtyList>
+
+      <Pagination>
+        <li>
+          <MdKeyboardArrowLeft size={16} />
+        </li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>
+          <MdKeyboardArrowRight size={16} />
+        </li>
+      </Pagination>
+    </>
+  );
 }
